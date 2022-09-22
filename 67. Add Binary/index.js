45b514e464aa35798751017e48781cc090698e26
Elijah Koulaxis <90087463+kx0101@users.noreply.github.com>
2022-09-22 15:19:49 +0300

    Create index.js

---
+++ 67. Add Binary/index.js
@@ -0,0 +1,6 @@
+var addBinary = function(a, b) {
+    return sum = (BigInt(`0b${a}`) + BigInt(`0b${b}`)).toString(2);
+};
+
+Runtime: 104 ms, faster than 48.42% of JavaScript online submissions for Add Binary.
+Memory Usage: 41.8 MB, less than 98.98% of JavaScript online submissions for Add Binary.
