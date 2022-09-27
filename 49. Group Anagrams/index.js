ae5eed4e079b95be3ca3442f289f53b39506fff6
Elijah Koulaxis <90087463+kx0101@users.noreply.github.com>
2022-09-27 23:28:00 +0300

    Create index.js

---
+++ 49. Group Anagrams/index.js
@@ -0,0 +1,14 @@
+var groupAnagrams = function (strs) {
+    let map = {}
+    
+    for (let i = 0; i < strs.length; i++) {
+        let sorted = strs[i].split('').sort().join();
+        
+        if (!map[sorted]) {
+            map[sorted] = [];
+        }
+        map[sorted].push(strs[i]);
+    }
+  
+    return Object.values(map);
+};
