9f70ce6bfc892cb7dc210acd14ddfa9f0c8811a4
Elijah Koulaxis <90087463+kx0101@users.noreply.github.com>
2022-09-28 19:08:31 +0300

    Create index.js

---
+++ 1822. Sign of the Product of an Array/index.js
@@ -0,0 +1,14 @@
+var arraySign = function (nums) {
+  let sign = 1;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === 0) {
+      return 0;
+    }
+
+    if (nums[i] < 0) {
+      sign = -sign;
+    }
+  }
+
+  return sign;
+};
