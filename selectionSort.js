c655251b1bee645daf9678ebb21c23b4af55d772
Elijah Koulaxis <90087463+kx0101@users.noreply.github.com>
2022-10-01 16:03:51 +0300

    Create selectionSort.js

---
+++ selectionSort.js
@@ -0,0 +1,18 @@
+function selectionSort(arr) {
+    for(let i = 0; i < arr.length; i++) {
+        let indexOfMin = i;
+        for(let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[indexOfMin]) {
+                indexOfMin = j;
+            }
+        }
+        
+        if (indexOfMin !== i) {
+            let temp = arr[indexOfMin];
+            arr[indexOfMin] = arr[i];
+            arr[i] = temp;
+        }
+    }
+    
+    return arr;
+}
