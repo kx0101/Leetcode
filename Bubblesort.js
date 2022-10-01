02c38645389175d6297270adb47bedbfd40a056c
Elijah Koulaxis <90087463+kx0101@users.noreply.github.com>
2022-10-01 15:51:32 +0300

    Update Bubblesort.js

--- Bubblesort.js
+++ Bubblesort.js
@@ -1,6 +1,6 @@
 function bubblesort(array) {
   for(let i = 0; i < array.length; i++) {
-    for(let j = 0; j < array.length - i; j++) {
+    for(let j = 0; j < array.length - i - 1; j++) {
       if (array[j] > array[j+1]) {
         let temp = array[j];
         array[j] = array[j+1];
