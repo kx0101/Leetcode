af26e797549925046a092d309e67ac25907672ca
Elijah Koulaxis <90087463+kx0101@users.noreply.github.com>
2022-10-03 12:37:25 +0300

    Create index.js

---
+++ 17. Letter Combinations of a Phone Number/index.js
@@ -0,0 +1,35 @@
+var letterCombinations = function(digits) {
+    let letters = {
+       '2': 'abc',
+       '3': 'def',
+       '4': 'ghi',
+       '5': 'jkl',
+       '6': 'mno',
+       '7': 'pqrs',
+       '8': 'tuv',
+       '9': 'wxyz'
+    }
+    
+    if (!digits.length) return [];
+    let res = [""];
+    let lettersTwo;
+    let temp; 
+    
+    for(let i = 0; i < digits.length; i++) {
+        lettersTwo = letters[digits[i]]
+        temp = [];
+        for(let j = 0; j < res.length; j++) {
+            for(let k = lettersTwo.length - 1; k >= 0; k--) {
+                if (k == 0) {
+                    res[j] += lettersTwo[k]
+                } else {
+                    temp.push(res[j] + lettersTwo[k]);
+                }
+            }
+        }
+        res.push(...temp);
+    }
+    
+    return res;
+    
+};
