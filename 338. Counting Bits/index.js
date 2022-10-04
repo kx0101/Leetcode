575b8ca14ebde6b01e8ae2d84f296e49865b05bc
Elijah Koulaxis <90087463+kx0101@users.noreply.github.com>
2022-10-04 16:01:53 +0300

    Create index.js

---
+++ 338. Counting Bits/index.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var countBits = function(n) {
+    let result = new Array(n + 1).fill(0);
+    let offset = 1;
+    
+    for(let i = 1; i < n + 1; i++) {
+        if (offset * 2 === i) {
+            offset = i;
+        }
+        result[i] = 1 + result[i - offset];
+    }
+    
+    return result;
+};
