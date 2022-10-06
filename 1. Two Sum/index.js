409e4dfd869717dc3ad03050796795568cdf7db9
Elijah Koulaxis <90087463+kx0101@users.noreply.github.com>
2022-10-06 12:52:36 +0300

    Create index.js

---
+++ 1. Two Sum/index.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const map = new Map();
+    
+    for(let i = 0; i < nums.length; i++) {
+        if (map.get(nums[i]) === undefined) {
+            map.set(target - nums[i], i);
+        } else {
+            return [map.get(nums[i]), i];
+        }
+    }
+    
+    return null;
+};
