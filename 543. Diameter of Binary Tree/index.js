c399549978dc50bc372d64b5086f4f0b80659d3e
Elijah Koulaxis <90087463+kx0101@users.noreply.github.com>
2022-10-06 15:02:20 +0300

    Create index.js

---
+++ 543. Diameter of Binary Tree/index.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var diameterOfBinaryTree = function(root) {
+    if (!root) return 0;
+    
+    let diameter = 0;
+    longestPath(root);
+    return diameter;
+    
+    function longestPath(node) {
+        if (node === null) return 0;
+        
+        let leftPath = longestPath(node.left);
+        let rightPath = longestPath(node.right);
+        
+        diameter = Math.max(diameter, rightPath + leftPath);
+        
+        return Math.max(leftPath, rightPath) + 1;
+    }
+    
+};
