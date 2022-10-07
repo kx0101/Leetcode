e649cde1c0b4e540993b5bdd5a12367cb3074ca3
Elijah Koulaxis <90087463+kx0101@users.noreply.github.com>
2022-10-07 16:04:14 +0300

    Update index.js

--- 49. Group Anagrams/index.js
+++ 49. Group Anagrams/index.js
@@ -1,14 +1,21 @@
-var groupAnagrams = function (strs) {
-    let map = {}
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    let map = {};
     
-    for (let i = 0; i < strs.length; i++) {
-        let sorted = strs[i].split('').sort().join();
-        
-        if (!map[sorted]) {
-            map[sorted] = [];
+    for(let str of strs) {
+        let charCount = new Array(26).fill(0);
+        for(let c of str) {
+            charCount[c.charCodeAt(0) - 'a'.charCodeAt(0)]++
         }
-        map[sorted].push(strs[i]);
+        
+        if (!map[charCount]) {
+            map[charCount] = [];
+        }
+        map[charCount].push(str);
     }
-  
+    
     return Object.values(map);
 };
