b6ba05a5b9342cb6c6563cf96367cb400f59bd3c
Elijah Koulaxis <90087463+kx0101@users.noreply.github.com>
2022-10-07 16:15:41 +0300

    Update index.js

--- 5. Longest Palindromic Substring/index.js
+++ 5. Longest Palindromic Substring/index.js
@@ -1,30 +1,34 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
 var longestPalindrome = function(s) {
     let result = ""
     let resultLength = 0;
     
     for(let i = 0; i < s.length; i++) {
-        let l = i;
-        let r = i;
+        let left = i;
+        let right = i;
         
-        while(l >= 0 && r < s.length && s[l] === s[r]) {
-            if ( (r - l + 1) > resultLength ) {
-                result = s.substring(l, r+1);
-                resultLength = r - l + 1
+        while (left >= 0 && right < s.length && s[left] === s[right]) {
+            if ((right - left + 1 > resultLength)) {
+                result = s.substring(left, right + 1);
+                resultLength = right - left + 1;
             }
-            l -= 1;
-            r += 1;
+            left--;
+            right++;
         }
         
-        l = i;
-        r = i + 1;
+        left = i;
+        right = i + 1;
         
-        while(l >= 0 && r < s.length && s[l] === s[r]) {
-            if ((r - l + 1) > resultLength) {
-                result = s.substring(l, r+1);
-                resultLength = r - l + 1;
+        while(left >= 0 && right < s.length && s[left] === s[right]) {
+            if ((right - left + 1) > resultLength) {
+                result = s.substring(left, right + 1);
+                resultLength = right - left + 1;
             }
-            l -= 1
-            r += 1
+            left--;
+            right++;
         }
     }
     
