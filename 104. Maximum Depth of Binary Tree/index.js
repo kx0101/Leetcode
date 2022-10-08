d87a7755b08ea73df2386035c902374a43878132
Elijah Koulaxis <90087463+kx0101@users.noreply.github.com>
2022-10-08 14:18:43 +0300

    Update index.js

--- 104. Maximum Depth of Binary Tree/index.js
+++ 104. Maximum Depth of Binary Tree/index.js
@@ -13,20 +13,9 @@
 var maxDepth = function(root) {
     if (!root) return 0;
     
-    let queue = [root];
-    let depth = 0;
-    let next = 0;
-    
-    while (queue.length) {
-        depth++;
-        next = queue.length;
-        for (let i = 0; i < next; i++) {
-            let current = queue.shift();
-            if (current.left) queue.push(current.left);
-            if (current.right) queue.push(current.right);
-        }
+    const calc = (node) => {
+        if (!node) return 0;
+        return Math.max(1 + calc(node.left), 1 + calc(node.right));
     }
-    
-    return depth;
-    
+    return calc(root);
 };
