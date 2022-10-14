b0d7a50fe4589e031dbed25308de20e8d37d8e69
Elijah Koulaxis <90087463+kx0101@users.noreply.github.com>
2022-10-14 15:05:43 +0300

    Update index.js

--- 977. Squares of a Sorted Array/index.js
+++ 977. Squares of a Sorted Array/index.js
@@ -5,9 +5,20 @@
 var sortedSquares = function(nums) {
     let result = [];
     
-    for(let i = 0; i < nums.length; i++) {
-        result.push(nums[i] * nums[i]);
+    let left = 0;
+    let right = nums.length - 1;
+    
+    for(let i = nums.length - 1; i >= 0; i--) {
+        let square;
+        if (Math.abs(nums[left]) < Math.abs(nums[right])) {
+            square = nums[right];
+            right--;
+        } else {
+            square = nums[left];
+            left++;
+        }
+        result[i] = square * square;
     }
     
-    return result.sort((x, y) => x - y);
+    return result;
 };
