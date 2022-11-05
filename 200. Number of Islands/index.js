90c4aae170f6846b47b894b780123664d2377db5
Elijah Koulaxis <90087463+kx0101@users.noreply.github.com>
2022-11-05 19:00:20 +0200

    Update index.js

--- 200. Number of Islands/index.js
+++ 200. Number of Islands/index.js
@@ -1,35 +1,33 @@
-// Since JS has no Priority Queue, I guess I will have to edit the data to mark the visited.
-
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
 var numIslands = function(grid) {
-    if (!grid) {
-        return 0;
-    }
+    if (!grid) return 0;
+    let counter = 0;
+    let visited = Array.from(Array(grid.length), () => Array(grid[0].length).fill(false));
     
-    let islands = 0;
-    
-    for(let r = 0; r < grid.length; r++) {
-        for (let c = 0; c < grid[0].length; c++) {
-            if (grid[r][c] === "1") {
-                dfs(r, c);
-                islands += 1;
+    for(let row = 0; row < grid.length; row++) {
+        for(let col = 0; col < grid[0].length; col++) {
+            if (grid[row][col] === "1" && visited[row][col] === false) {
+                counter++;
+                dfs(grid, row, col, visited);
             }
         }
     }
     
-    return islands;
+    return counter;
     
-    function dfs(row, col) {
-        if (row < 0 || col < 0 || row > grid.length - 1 || col > grid[0].length - 1 || grid[row][col] === "0") {
-            return;
-        }
+    function dfs(grid, row, col, visited) {
+        if (row < 0 || row === grid.length || col < 0 || col === grid[0].length || visited[row][col] === true) return;
         
-        grid[row][col] = "0";
+        if (grid[row][col] === "1") visited[row][col] = true;
+        else return;
         
-        dfs(row + 1, col);
-        dfs(row - 1, col);
-        dfs(row, col + 1);
-        dfs(row, col - 1);
-        
+        dfs(grid, row, col + 1, visited);
+        dfs(grid, row, col - 1, visited);
+        dfs(grid, row + 1, col, visited);
+        dfs(grid, row - 1, col, visited);
     }
     
 };
