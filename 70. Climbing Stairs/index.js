4ccb88d32ddd86758cf2023399cb9ad91cbe8cbe
Elijah Koulaxis <90087463+kx0101@users.noreply.github.com>
2022-11-17 11:23:35 +0200

    Update index.js

--- 70. Climbing Stairs/index.js
+++ 70. Climbing Stairs/index.js
@@ -3,17 +3,13 @@
  * @return {number}
  */
 var climbStairs = function(n) {
-    if (n == 1) {
-        return 1;
+    let [one, two] = [1, 1];
+    
+    for(let i = 0; i < n - 1; i++) {
+        let temp = one;
+        one = one + two;
+        two = temp;
     }
     
-    let result = new Array(n);
-    result[1] = 1;
-    result[2] = 2;
-    
-    for(let i = 3; i <= n; i++) {
-        result[i] = result[i - 1] + result[i - 2];
-    }
-    
-    return result[n];
+    return one;
 };
