91d739bc4293324758ee573e54d7961c1f74294d
Elijah Koulaxis <90087463+kx0101@users.noreply.github.com>
2022-11-21 18:55:49 +0200

    Create index.js

---
+++ 2. Add Two Numbers/index.js
@@ -0,0 +1,23 @@
+var addTwoNumbers = function(l1, l2) {
+    let dummy = new ListNode(0);
+    let curr = dummy;
+    
+    let carry = 0;
+    
+    while (l1 || l2 || carry) {
+        let v1 = l1 ? l1.val : 0;
+        let v2 = l2 ? l2.val : 0;
+        
+        let val = v1 + v2 + carry;
+        carry = Math.floor(val / 10);
+        val = val % 10;
+        curr.next = new ListNode(val);
+        
+        curr = curr.next;
+        
+        l1 = l1 ? l1.next : null;
+        l2 = l2 ? l2.next : null;
+    }
+    
+    return dummy.next;
+};
