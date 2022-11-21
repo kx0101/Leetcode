438a1cd341d9e331a16e64ecf6f54e4fa1b7f6dd
Elijah Koulaxis <90087463+kx0101@users.noreply.github.com>
2022-11-21 13:48:43 +0200

    Update index.js

--- 207. Course Schedule/index.js
+++ 207. Course Schedule/index.js
@@ -5,43 +5,33 @@
  */
 var canFinish = function(numCourses, prerequisites) {
     let preMap = new Map();
+    let visitedSet = new Set();
     
     for(let i = 0; i < numCourses; i++) {
         preMap.set(i, []);
     }
     
-    for(const [crs, pre] of prerequisites) {
+    for(let [crs, pre] of prerequisites) {
         preMap.get(crs).push(pre);
     }
     
-    let visitedSet = new Set();
-    
     function dfs(crs) {
-        if (visitedSet.has(crs)) {
-            return false;
-        }
-        
-        if (preMap.get(crs).length === 0) {
-            return true;
-        }
+        if (visitedSet.has(crs)) return false;
+        if (preMap.get(crs).length === 0) return true;
         
         visitedSet.add(crs);
         
         for(let pre of preMap.get(crs)) {
-            if (!dfs(pre)) {
-                return false;
-            }
+            if (!dfs(pre)) return false;
         }
-        visitedSet.delete(crs);
-        preMap.set(crs, [])
         
+        visitedSet.delete(crs);
+        preMap.set(crs, []);
         return true;
     }
     
     for(let i = 0; i < numCourses; i++) {
-        if (!dfs(i)) {
-            return false;
-        }
+        if (!dfs(i)) return false;
     }
     
     return true;
