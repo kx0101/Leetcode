11720001b7c97a525214af0a4ecf8c986a98cc3e
Elijah Koulaxis <90087463+kx0101@users.noreply.github.com>
2022-11-22 18:02:53 +0200

    Create index.js

---
+++ 443. String Compression/index.js
@@ -0,0 +1,31 @@
+/**
+ * @param {character[]} chars
+ * @return {number}
+ */
+var compress = function(chars) {
+    if (chars.length === 1) return 1;
+    let index = 0;
+    let i = 0;
+    
+    while (i < chars.length) {
+        let j = i;
+        while (j < chars.length && chars[j] === chars[i]) {
+            j++;
+        }
+        
+        chars[index++] = chars[i];
+        
+        if (j - i > 1) {
+            let count = j - i + "";
+            for(let c of count) {
+              chars[index++] = c;
+            }
+        }
+        
+        i = j;
+    }
+        
+    return index;
+};
+
+compress(["a","a","b","b","c","c","c"]);
