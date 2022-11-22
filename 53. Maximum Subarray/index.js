8d31a3baac69c8a19ae8923c3546e3100d12bef9
Elijah Koulaxis <90087463+kx0101@users.noreply.github.com>
2022-11-22 18:06:44 +0200

    Update index.js

--- 53. Maximum Subarray/index.js
+++ 53. Maximum Subarray/index.js
@@ -6,11 +6,9 @@ var maxSubArray = function(nums) {
     let currentSubArray = nums[0];
     let maxSubArray = nums[0];
     
-    for(let i = 1; i < nums.length; i++) {
-        let num = nums[i];
-        
-        currentSubArray = Math.max(num, currentSubArray + num);
-        maxSubArray = Math.max(maxSubArray, currentSubArray);
+    for(let i = 1 ; i < nums.length; i++) {
+        currentSubArray = Math.max(nums[i], currentSubArray + nums[i]);
+        maxSubArray = Math.max(currentSubArray, maxSubArray);
     }
     
     return maxSubArray;
