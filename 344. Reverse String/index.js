50fca436019bd74d8c44357ac59b8c1b3f530696
Elijah Koulaxis <90087463+kx0101@users.noreply.github.com>
2022-12-03 20:14:46 +0200

    Create index.js

---
+++ 344. Reverse String/index.js
@@ -0,0 +1,15 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    let [left, right] = [0, s.length - 1];
+    
+    while (left < right) {
+        let temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+        left++;
+        right--;
+    }
+};
