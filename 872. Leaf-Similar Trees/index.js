653978653d402f02f48f0383712a04b03b063578
Elijah Koulaxis <90087463+kx0101@users.noreply.github.com>
2022-12-08 17:30:28 +0200

    Create index.js

---
+++ 872. Leaf-Similar Trees/index.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root1
+ * @param {TreeNode} root2
+ * @return {boolean}
+ */
+var leafSimilar = function(root1, root2) {
+    function dfs(node, leaves) {
+        if (node) {
+            if (!node.left && !node.right) {
+                leaves.push(node.val);
+            }
+            dfs(node.left, leaves);
+            dfs(node.right, leaves);
+        }
+    }
+
+    let leaves1 = [];
+    let leaves2 = [];
+    dfs(root1, leaves1);
+    dfs(root2, leaves2);
+
+    return (leaves1.length === leaves2.length && leaves1.every((v, i) => v === leaves2[i]));
+};
