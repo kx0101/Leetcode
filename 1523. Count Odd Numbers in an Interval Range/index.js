4a608bd9dd500dbe846dda738634b71483e56ac2
Elijah Koulaxis <90087463+kx0101@users.noreply.github.com>
2023-02-13 16:38:31 +0200

    Create index.js

---
+++ 1523. Count Odd Numbers in an Interval Range/index.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+var countOdds = function(low, high) {
+    return Math.floor((high - low) / 2) + (low % 2 || high % 2);
+};
