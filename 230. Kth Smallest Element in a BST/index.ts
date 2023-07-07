cc01ee1d2d3757386245b3243705f024d5669570
Elijah Koulaxis <90087463+kx0101@users.noreply.github.com>
2023-07-07 18:26:59 +0300

    Update index.ts

--- 230. Kth Smallest Element in a BST/index.ts
+++ 230. Kth Smallest Element in a BST/index.ts
@@ -1,39 +1,17 @@
-/**
- * Definition for a binary tree node.
- * class TreeNode {
- *     val: number
- *     left: TreeNode | null
- *     right: TreeNode | null
- *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.left = (left===undefined ? null : left)
- *         this.right = (right===undefined ? null : right)
- *     }
- * }
- */
-
 function kthSmallest(root: TreeNode | null, k: number): number {
-    if (!root) {
-        return -1;
-    }
-
     let stack: TreeNode[] = [];
 
-    while (root || stack.length) {
-        while (root) {
-            stack.push(root);
-            root = root.left;
+    function dfs(root: TreeNode | null): void {
+        if (!root) {
+            return;
         }
 
-        root = stack.pop();
-        k--;
-
-        if (k === 0) {
-            return root.val;
-        }
-
-        root = root.right;
+        dfs(root.left);
+        stack.push(root);
+        dfs(root.right);
     }
 
-    return -1;
+    dfs(root);
+
+    return stack[k - 1].val;
 };
